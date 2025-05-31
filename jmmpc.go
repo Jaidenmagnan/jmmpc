69ff1110558fbfe6c2a6df4d7bc29c06f1627083
Jaiden Magnan <jaidenmagnan@gmail.com>
2025-05-30 23:20:31 -0400

    trying to find branch things

--- jmmpc.go
+++ jmmpc.go
@@ -3,73 +3,80 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/fhs/gompd/v2/mpd"
 )
 
-var baseStyle = lipgloss.NewStyle().
-	BorderStyle(lipgloss.NormalBorder()).
-	BorderForeground(lipgloss.Color("240"))
+// var baseStyle = lipgloss.NewStyle().
+// 	BorderStyle(lipgloss.NormalBorder()).
+// 	BorderForeground(lipgloss.Color("240"))
 
-type model struct {
-	songs    []string
-	cursor   int
-	selected map[int]struct{}
-}
+// type model struct {
+// 	songs    []string
+// 	cursor   int
+// 	selected map[int]struct{}
+// }
 
-func initialModel(songs []string) model {
-	return model{
-		songs:    songs,
-		selected: make(map[int]struct{}),
-	}
-}
+//func makeTable(artists []string, songs []string) {
+//	columns := []table.Column{
+//		{Title: "Song Name", Width: 20},
+//		{Title: "Artist", Width: 4},
+//	}
+//
+//	rows := table[row]
+//
+//}
 
-func (m model) View() string {
-	s := "Songs Listed?\n\n"
-	for i, song := range m.songs {
-		cursor := " "
-		if m.cursor == i {
-			cursor = ">"
-		}
-		s += fmt.Sprintf("%s %s\n", cursor, song)
-	}
-	s += "\nPress q to quit.\n"
+// func initialModel(songs []string) model {
+// 	return model{
+// 		songs:    songs,
+// 		selected: make(map[int]struct{}),
+// 	}
+// }
 
-	return s
-}
+// func (m model) View() string {
+// 	s := "Songs Listed?\n\n"
+// 	for i, song := range m.songs {
+// 		cursor := " "
+// 		if m.cursor == i {
+// 			cursor = ">"
+// 		}
+// 		s += fmt.Sprintf("%s %s\n", cursor, song)
+// 	}
+// 	s += "\nPress q to quit.\n"
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		case "down", "j":
-			if m.cursor < len(m.songs)-1 {
-				m.cursor++
-			}
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
-		}
-	}
-	return m, nil
-}
+// 	return s
+// }
 
-func (m model) Init() tea.Cmd {
-	return nil
-}
+// func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+// 	switch msg := msg.(type) {
+// 	case tea.KeyMsg:
+// 		switch msg.String() {
+// 		case "ctrl+c", "q":
+// 			return m, tea.Quit
+// 		case "up", "k":
+// 			if m.cursor > 0 {
+// 				m.cursor--
+// 			}
+// 		case "down", "j":
+// 			if m.cursor < len(m.songs)-1 {
+// 				m.cursor++
+// 			}
+// 		case "enter", " ":
+// 			_, ok := m.selected[m.cursor]
+// 			if ok {
+// 				delete(m.selected, m.cursor)
+// 			} else {
+// 				m.selected[m.cursor] = struct{}{}
+// 			}
+// 		}
+// 	}
+// 	return m, nil
+// }
+
+// func (m model) Init() tea.Cmd {
+// 	return nil
+// }
 
 func main() {
 	var err error
@@ -81,20 +88,24 @@ func main() {
 
 	defer conn.Close()
 
-	songs, err := conn.GetFiles()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	_, err = conn.Update("")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	p := tea.NewProgram(initialModel(songs))
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
+	songs, err := conn.ListAllInfo("/")
+	if err != nil {
+		log.Fatalln(err)
 	}
+
+	for _, song := range songs {
+		fmt.Println(song)
+	}
+
+	//p := tea.NewProgram(initialModel(songs))
+	//if _, err := p.Run(); err != nil {
+	//	fmt.Printf("Alas, there's been an error: %v", err)
+	//	os.Exit(1)
+	//}
 
 }
