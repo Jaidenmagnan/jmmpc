71305811e2586fa0c47a4974bbff25eff6e7c6b7
Jaiden Magnan <jaidenmagnan@gmail.com>
2025-05-30 22:34:41 -0400

    project

---
+++ jmmpc.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/fhs/gompd/v2/mpd"
+)
+
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+type model struct {
+	songs    []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func initialModel(songs []string) model {
+	return model{
+		songs:    songs,
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) View() string {
+	s := "Songs Listed?\n\n"
+	for i, song := range m.songs {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+		s += fmt.Sprintf("%s %s\n", cursor, song)
+	}
+	s += "\nPress q to quit.\n"
+
+	return s
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.songs)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func main() {
+	var err error
+
+	conn, err := mpd.Dial("tcp", "localhost:6600")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer conn.Close()
+
+	songs, err := conn.GetFiles()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	_, err = conn.Update("")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	p := tea.NewProgram(initialModel(songs))
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+
+}
